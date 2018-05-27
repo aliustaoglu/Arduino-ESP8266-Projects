e493b7cda53410957e5a5ddef90cf2607ffe8f1e
Cuneyt Aliustaoglu <aliustaoglu@yahoo.com>
2018-05-27 21:36:01 +1200

    c cpp properties for mac

--- Alcoholmeter-Nano-MQ3-Oled/Alcoholmeter-Nano-MQ3-Oled.ino
+++ Alcoholmeter-Nano-MQ3-Oled/Alcoholmeter-Nano-MQ3-Oled.ino
@@ -1,41 +1,70 @@
-
 #include "U8glib.h"
 #include <math.h>
 
+// Nokia 5110
+// U8GLIB_PCD8544 u8g(11, 10, 8, 9, 7); // Clk, Din, DC, CE, RST
 
+// .96 OLED
 U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE|U8G_I2C_OPT_DEV_0);  // I2C / TWI 
 
-const int AOUTpin=0;
-const int ledPin=13;
+
+const int AOUTpin = 0;
+const int DOUTpin = 8;
+const int ledPin = 13;
 
 int limit;
 int value;
 
+void draw()
+{
+  int num = analogRead(AOUTpin);
+  int ortalama = round((num * 84) / 1024);
 
-void sayfaYazdir() {
-  u8g.setFont(u8g_font_unifont);
-  u8g.drawStr( 0, 22, "Merhaba Cuneyt");
-  
-  
-  value= analogRead(AOUTpin);
-  Serial.println(value);
+  char buffer[7];
 
-  delay(1000);
+  u8g.setFont(u8g_font_7x14);
+  u8g.drawStr(0, 36, "Alcohol:");
+  u8g.drawStr(60, 36, itoa(num, buffer, 10));
+
+  u8g.drawRFrame(0, 0, 84, 24, 0.9);
+  u8g.drawBox(0, 0, ortalama, 24);
+
+  if (value > 100)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
 }
 
-void setup(void) {
+void setup()
+{
+  u8g.setColorIndex(1);
+  //u8g.setRot180();
   Serial.begin(115200);
-  pinMode(AOUTpin, INPUT);
+  pinMode(DOUTpin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
-void loop(void) {
-  // picture loop
+void loop()
+{
   u8g.firstPage();
-  int i = 0;
-  do {
-    sayfaYazdir();
-  } while( u8g.nextPage() );
+
+  do
+  {
+    draw();
+  } while (u8g.nextPage());
+
+  /*value = analogRead(AOUTpin);
+  limit = digitalRead(DOUTpin);
+  Serial.print("Alcohol value: ");
+  Serial.println(value);
+  Serial.print("Limit: ");
+  Serial.print(limit);
+  delay(1000);*/
   
+
   delay(1000);
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Projects and Sketches for Arduino, ESP8266, WEMOS, NodeMCU and Atttiny85
+
+
+Debug Arduino 
+
+1. Download and install d2xx drivers http://www.ftdichip.com/Drivers/D2XX/MacOSX/D2XX1.0.4.dmg (from http://www.ftdichip.com/Drivers/D2XX.htm)
+
+2. Install OpenOCD from homebrew (http://mxcl.github.com/homebrew/)
+
+  $ brew install openocd --enable-ft2232_libftdi
+
+OPEN OCD
+/usr/local/Cellar/open-ocd/0.10.0/bin 
